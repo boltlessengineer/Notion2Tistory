ddd928c97187b4c984374b975eae9dc8d4141642
seongmin8452 <boltlessengineer@gmail.com>
2020-09-03 15:18:50 +0900

    [TEST] myUpdateChecker 테스트 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const { autoUpdater } = require("electron-updater");
+const { checkUpdate } = require("./myUpdateChecker");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -8,11 +8,9 @@ if (require("electron-squirrel-startup")) {
     app.quit();
 }
 
-let mainWindow;
-
 const createWindow = () => {
     // Create the browser window.
-    mainWindow = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
         width: 460,
         height: 320,
         webPreferences: {
@@ -29,59 +27,13 @@ const createWindow = () => {
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
 };
-const sendStatusToWindow = (text) => {
-    mainWindow.webContents.send("message", text);
-};
-
-autoUpdater.on("cheking-for-update", () => {
-    sendStatusToWindow("Checking for update...");
-});
-autoUpdater.on("update-available", (info) => {
-    console.log(info);
-    sendStatusToWindow("Update available.");
-});
-autoUpdater.on("update-not-available", (info) => {
-    console.log(info);
-    sendStatusToWindow("Update not available.");
-});
-autoUpdater.on("error", (err) => {
-    sendStatusToWindow("Error in auto-updater. " + err);
-});
-autoUpdater.on("download-progress", (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + " - Downloaded " + progressObj.percent + "%";
-    log_message =
-        log_message +
-        " (" +
-        progressObj.transferred +
-        "/" +
-        progressObj.total +
-        ")";
-    sendStatusToWindow(log_message);
-});
-autoUpdater.on("update-downloaded", (info) => {
-    sendStatusToWindow("Update downloaded");
-
-    const option = {
-        type: "question",
-        buttons: ["업데이트", "취소"],
-        defaultId: 0,
-        title: "electron-updater",
-        message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
-    };
-    let btnIndex = dialog.showMessageBoxSync(updateWin, option);
-
-    if (btnIndex === 0) {
-        autoUpdater.quitAndInstall();
-    }
-});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
     createWindow();
-    autoUpdater.checkForUpdates();
+    checkUpdate();
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
